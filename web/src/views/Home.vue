f636ac218b444ff5489632f3bdd7d7d84068efeb
Samuel <sam@Samuels-MacBook-Pro.local>
2021-02-16 17:34:45 +0100

    adding vue web app, with bulma setup, vuex and sample uses for bulma / vuex store setup

---
+++ web/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <HelloWorld v-bind:msg="getUser.name" />
+    <button class="button is-primary" v-on:click="changeName()">Execute a store action</button>
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+import {Options, Vue} from 'vue-class-component';
+import {mapActions} from 'vuex';
+
+@Options({
+  components: {
+    HelloWorld
+  },
+  computed: {
+    getUser() {
+      return this.$store.state.testModule.user;
+    }
+  },
+  methods: {
+    // Get action changeName from testModule store
+    ...mapActions('testModule', ['changeName'])
+  }
+})
+export default class Home extends Vue {}
+</script>
