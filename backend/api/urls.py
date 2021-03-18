e1515bfb841a55b5c1f23e08c103d19091d4afb9
PetitSamuel <petits@tcd.ie>
2021-03-18 14:30:47 +0100

    fix API - now compiles

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from api import views
+from api import api
 
 urlpatterns = [
-    # TODO : fix this (currently does not compile)
-    # path('test/', api.test, name='test'),
-    # path('status/', api.status, name='status'),
-    # path('api/auth', api.auth, name='auth'),
-    # path('api/create_business', api.create_business, name='create_business'),
+    path('test', api.test, name='test'),
+    path('status', api.status, name='status'),
+    path('auth', api.auth, name='auth'),
+    path('create_business', api.create_business, name='create_business'),
 ]
