dd8246ee983e826c978b359b058a4ce97f958d2e
Liam Sherlock <liam@thamlock.com>
2021-03-23 10:32:02 +0000

    added db to dev.sh also package.json was broken so fixed that

--- develop.sh
+++ develop.sh
@@ -4,8 +4,10 @@ if [[ $(tmux ls)  == *"barterDev"* ]]; then
   tmux kill-session -t barterDev
 fi
 tmux new-session -d -s barterDev -n components 
-tmux send-keys -t barterDev 'cd backend; clear; python manage.py runserver' 'C-m'
+tmux send-keys -t barterDev './run_frontend.sh' 'C-m'
 tmux split-window -t barterDev 
-tmux send-keys -t barterDev 'cd web; clear; npm run serve' 'C-m'
+tmux send-keys -t barterDev './run_backend.sh' 'C-m'
+tmux split-window -h -t barterDev 
+tmux send-keys -t barterDev './run_db.sh' 'C-m'
 
 tmux attach-session -t barterDev
