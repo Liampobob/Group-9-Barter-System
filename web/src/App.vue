17db1f5cfe11eca6af15ab92ab89e0e385485daa
sarah <sastaffo@tcd.ie>
2021-03-31 13:33:23 +0100

    adding 'newlisting' to App.vue router

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,8 +5,8 @@
     <router-link to="/listings">Listings</router-link> |
     <router-link to="/search">Search</router-link> |
     <router-link to="/profile">Profile</router-link> |
-    <router-link to="/signout">Sign Out</router-link>
+    <router-link to="/signout">Sign Out</router-link> |
+    <router-link to="/newlisting">New Listing</router-link>
     <router-view />
   </div>
 </template>
-
