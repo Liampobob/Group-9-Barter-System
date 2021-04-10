ce5fa3103767449890a5d92478d52c2c5de5199e
PetitSamuel <petits@tcd.ie>
2021-04-10 20:05:07 +0200

    implement patch server side

--- backend/api/api_views/workers.py
+++ backend/api/api_views/workers.py
@@ -3,6 +3,7 @@ from rest_framework import status as status_codes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from query.models import User
+import json
 
 
 class WorkerAPI(generics.CreateAPIView):
@@ -17,6 +18,7 @@ class WorkerAPI(generics.CreateAPIView):
         print(user)
         return JsonResponse({'user': user.to_dict()}, status=status_codes.HTTP_200_OK)
 
+
 class UserAPI(generics.CreateAPIView):
     """User API Class"""
     permission_classes = [IsAuthenticated]  # require auth
@@ -27,4 +29,26 @@ class UserAPI(generics.CreateAPIView):
             return JsonResponse({'error': 'username not found'}, status=status_codes.HTTP_400_BAD_REQUEST)
         user = User.objects.get(username=username)
         print(user)
+        return JsonResponse({'user': user.to_dict()}, status=status_codes.HTTP_200_OK)
+
+    def patch(self, request):
+        data = json.loads(request.body)
+        if not data.get('username') or not data.get('name') or not data.get('phone_number'):
+            return JsonResponse({'error': 'expected fields: username name phone_number (bio is optional)'}, status=status_codes.HTTP_403_FORBIDDEN)
+
+        user = User.objects.get(id=request.user.id)
+        if not user:
+            return JsonResponse({'error': 'user not found'}, status=status_codes.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        user.username = data['username']
+        user.name = data['name']
+        user.phone_number = data['phone_number']
+        user.bio = data['bio']
+
+        try:
+            user.save(update_fields=['username',
+                      'name', 'bio', 'phone_number'])
+        except Exception as e:
+            return JsonResponse({'error': 'error saving changes', 'details': e}, status=status_codes.HTTP_500_INTERNAL_SERVER_ERROR)
+
         return JsonResponse({'user': user.to_dict()}, status=status_codes.HTTP_200_OK)
